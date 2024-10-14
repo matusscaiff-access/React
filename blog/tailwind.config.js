be75f9dc99d0eff1f9e75a92be5873cafba1944a
matusscaiff-access <matus.scaiff.356@accesscreative.ac.uk>
2024-10-14 15:50:56 +0100

    hsl colours without integration into main code

--- blog/tailwind.config.js
+++ blog/tailwind.config.js
@@ -6,7 +6,12 @@ export default {
   ],
   theme: {
     screens:{ //tailwind screen sizes so that you can adjust the colours for different screen sizes
-      sm:"400px",
+      sm:"480",
+      sml:"768",
+      md:"834",
+      mdl:"1024",
+      lg:"1280",
+      xl:"1440",
     },
     extend: {
       fontFamily:{ // importing fonts and naming them
@@ -16,7 +21,29 @@ export default {
       colors:{ // colours and their hex codes, you can name each colour so that it is easy to reuse later
         teal: "#488286",
         tiffany: "#A8DCDA",
-        onyx: "#373E40"
+        onyx: "#373E40",
+
+        teal:{
+          100: "hsl(184, 30, 60)",
+          200: "hsl(184, 30, 50)",
+          300: "hsl(184, 30, 40)",
+          400: "hsl(184, 30, 30)",
+          500: "hsl(184, 30, 20)",
+        },
+        tiffany:{
+          100: "hsl(178, 43, 96)",
+          200: "hsl(178, 43, 86)",
+          300: "hsl(178, 43, 76)",
+          400: "hsl(178, 43, 66)",
+          500: "hsl(178, 43, 56)",
+        },
+        onyx: {
+          100: "hsl(193, 8%, 43%)",
+          200: "hsl(193, 8%, 33%)",
+          300: "hsl(193, 8%, 23%)",
+          400: "hsl(193, 8%, 13%)",
+          500: "hsl(193, 8%, 3%)",
+        }
       }
     },
   },
